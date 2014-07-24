81214c5d3f4d50ad4c5d728ead62449b24a8e1ac
Jeff Mahoney <jeffm@jeffreymahoney.com>
2014-07-24 15:43:16 -0400

    linkcache.sqlite3: don't expect connections to persist
    Supybot runs multithreaded and creating a connection in one thread while
    using it another really makes the sqlite module angry.
    
    Signed-off-by: Jeff Mahoney <jeffm@jeffreymahoney.com>

--- linkcache/database/sqlite3.py
+++ linkcache/database/sqlite3.py
@@ -13,32 +13,42 @@ class LinkSqlite(sql.LinkSql):
         self.filename = config['filename']
         self.db = None
         self.retries = 1
-        self.connect()
-
-    def connect(self):
         create = os.path.exists(self.filename) is False
-        self.db = sqlite3.connect(self.filename,
-                                  detect_types=sqlite3.PARSE_COLNAMES)
+
+        self.connect()
         if create:
             self.create_tables()
-
         self.confirm_table()
 
+        self.close()
+
+    def connect(self):
+        if self.db is None:
+            self.db = sqlite3.connect(self.filename,
+                                      detect_types=sqlite3.PARSE_COLNAMES)
+    def close(self):
+        if self.db:
+            self.db.close()
+            self.db = None
+
     def execute(self, command, args):
+        self.connect()
         cursor = self.db.cursor()
         cursor.execute(command, args)
         self.db.commit()
+        self.close()
 
+    # Leaves the connection open since the fetch will require it
     def query(self, command, args):
         for i in range(0, self.retries + 1):
             try:
+                self.connect()
                 cursor = self.db.cursor()
                 cursor.execute(command, args)
             except sqlite3.OperationalError, e:
                 if i < self.retries:
                     print e
-                    self.db.close()
-                    self.db = None
+                    self.close()
                     self.connect()
                 else:
                     raise
@@ -49,11 +59,15 @@ class LinkSqlite(sql.LinkSql):
 
     def query_one(self, query, args):
         cursor = self.query(query, args)
-        return cursor.fetchone()
+        result = cursor.fetchone()
+        self.close()
+        return result
 
     def query_all(self, query, args):
         cursor = self.query(query, args)
-        return cursor.fetchall()
+        result = cursor.fetchall()
+        self.close()
+        return result
 
     def describe(self, table):
         res = self.query_all("PRAGMA TABLE_INFO(%s)" % table, ())
